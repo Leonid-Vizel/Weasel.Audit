95303e197137613d8dff8a427cc5ca0723300299
Vizel Leonid <spartanlaska@gmail.com>
2023-11-17 15:22:22 +0300

    Export update

---
+++ Weasel.Export.Common/Interfaces/Rows/IOneToManyRowExporter.cs
@@ -0,0 +1,8 @@
+﻿using Weasel.Export.Common.Models;
+
+namespace Weasel.Export.Common.Interfaces.Rows;
+
+public interface IOneToManyRowExporter<T>
+{
+    IReadOnlyCollection<StandartRow> ToRows(T data, ref int counter);
+}
---
+++ Weasel.Export.Common/Interfaces/Rows/IStandartRowExporter.cs
@@ -0,0 +1,8 @@
+﻿using Weasel.Export.Common.Models;
+
+namespace Weasel.Export.Common.Interfaces.Rows;
+
+public interface IStandartRowExporter<T>
+{
+    StandartRow ToRow(T data, ref int counter);
+}
