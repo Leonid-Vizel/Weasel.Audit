f6a566de70282eb274d5f9c168e70b2996156752
Vizel Leonid <spartanlaska@gmail.com>
2023-12-01 17:28:04 +0300

    Export update for razor pages

--- Weasel.Export.AspNetCore/ExportExtensions.cs
+++ Weasel.Export.AspNetCore/ExportExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Weasel.Export.AspNetCore;
 
@@ -7,4 +8,6 @@ public static class ExportExtensions
     private static readonly string _xlMimeFormat = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
     public static FileContentResult XlFile(this Controller controller, byte[] result, string name)
         => controller.File(result, _xlMimeFormat, $"{name}.xlsx");
+    public static FileContentResult XlFile(this PageModel page, byte[] result, string name)
+        => page.File(result, _xlMimeFormat, $"{name}.xlsx");
 }
